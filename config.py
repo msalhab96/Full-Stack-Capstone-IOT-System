ee27f844c350f3b3b90e24126ee4ce2557307cb6
Mahmoud Salhab <mahmoud@salhab.work>
2021-02-24 19:46:18 +0200

    database path changed

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ AUTH0_DOMAIN = "deviceiot.au.auth0.com"
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'deviceapi'
 
-DATABASE_PATH = "ostgres://tavextxdjjjftk:9d8ac1cb80be91f11fc2f8f755d9a3dda1b1b4562424f0d090f3eeff28596b46@ec2-3-209-176-42.compute-1.amazonaws.com:5432/de1dt92bf1fcia"
+DATABASE_PATH = "postgres://tavextxdjjjftk:9d8ac1cb80be91f11fc2f8f755d9a3dda1b1b4562424f0d090f3eeff28596b46@ec2-3-209-176-42.compute-1.amazonaws.com:5432/de1dt92bf1fcia"
