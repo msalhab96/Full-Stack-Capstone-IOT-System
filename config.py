cf4b9ef6860dec979ba3dea6b347f1b2a38f6703
Mahmoud Salhab <mahmoud@salhab.work>
2021-02-24 20:12:45 +0200

    -

--- config.py
+++ config.py
@@ -7,4 +7,9 @@ AUTH0_DOMAIN = "deviceiot.au.auth0.com"
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'deviceapi'
 
-DATABASE_PATH = "postgres://xtnbbwqriiovhh:8b2ed9735a0bcecbf5681ac2911f9d93aefc417933bdf06027953add92129ead@ec2-54-144-251-233.compute-1.amazonaws.com:5432/d8ajlqq6rtn6ur"
+DATABASE_PATH = "postgres://{}:{}@{}/{}".format(
+                                                USERNAME, 
+                                                PASSWORD, 
+                                                END_POINT, 
+                                                DATABASE_NAME
+                                                )
